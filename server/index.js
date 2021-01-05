7b7894d12a3f4071353df76a92f6c9794c4596f5
kam237zasada <kam237zasada@wp.pl>
2021-01-05 08:03:23 +0100

    Change security with database

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors')
 const users = require('./routes/users')
+const mongoLink = process.env.DBCONNECTION
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
@@ -13,7 +14,7 @@ app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`))
 
-mongoose.connect('mongodb+srv://kam237zasada:kam237zasada@cluster0.mz2cl.mongodb.net/test', { useFindAndModify: false, useNewUrlParser: true,useUnifiedTopology: true })
+mongoose.connect(mongoLink, { useFindAndModify: false, useNewUrlParser: true,useUnifiedTopology: true })
     .then (() => console.log("Connected.."))
     .catch (err => console.error(err));
 
